a6b99771d3a973bdd4ce890c57112decfc61ce45
orwenn22 <46846090+orwenn22@users.noreply.github.com>
2022-12-10 15:53:33 +0100

    Move the CLI stuff in its own file

---
+++ src/CLI.h
@@ -0,0 +1,9 @@
+#ifndef CLI_H
+#define CLI_H
+
+void OpenEditor(int argc, const char* argv[]);
+int ResizeCommand(int argc, const char* argv[]);
+int ExpandCommand(int argc, const char* argv[]);
+int HelpCommand(int argc, const char* argv[]);
+
+#endif
